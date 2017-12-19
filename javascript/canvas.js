337d2daa4e99f097cd1425d15688181e56ccbb3f
ArnoFr06 <olivieri06.a@gmail.com>
2017-12-19 14:59:04 +0100

    Add files via upload

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,35 +1,55 @@
 window.onload = init;
-
+var gf;
 let canvas, ctx;
 
-function init(){
-  canvas = document.querySelector("#myCanvas");
-  ctx = canvas.getContext("2d");
+function init() {
+	gf = new GameFramework();
+	gf.init();
 }
+
+
+
+function GameFramework(){
+
+  let canvas, ctx, size;
+  let tirs = [];
+  let bosses = [];
+
+  function init(){
+    canvas = document.querySelector("#myCanvas");
+    ctx = canvas.getContext("2d");
+    requestAnimationFrame(anime)
+  }
+
+}
+
+
+
+
+
 class Personnage{
-  
-  constructor(nom, vie, posx, posy, vx, vy, l, h) {
-		this.nom = nom; 
+
+  constructor(nom, vie, posx, posy, vx, vy, size) {
+		this.nom = nom;
 		this.vie = vie; //0 à 100
 		this.positionX = posx;
-        this.positionY = posy;
+    this.positionY = posy;
 		this.vitesseX = vx;
-        this.vitesseY = vy;
-		this.width = l;
-		this.height = h;
+    this.vitesseY = vy;
+		this.size = size;
 	}
   function deplacement(){
-   
-     } 
+
+     }
 
   function perteVie(val){
     this.vie-=val;
   }
-  
+
   function vie(){
     return this.vie;
   }
-  
+
    touchesWall(){
     if (
         this.center.x - 0.5 * this.size < 0 ||
@@ -55,43 +75,43 @@ class Personnage{
         return false;
     }
   }
-  
-  
+
+
 }
 
 class Joueur extends Personnage{
-  
+
     constructor(posx, posy, coul, vx, vy, l, h) {
 		super(posx, posy, coul, vx, vy, l, h);
-		
+
 	}
-  
+
   function deplacement(){
-      var key_pressed; 
+      var key_pressed;
      if(event == null){
-          key_pressed = window.event.keyCode; 
+          key_pressed = window.event.keyCode;
      }
      else {
-          key_pressed = event.keyCode; 
+          key_pressed = event.keyCode;
      }
      switch(key_pressed){
           case 37:
                left=true;
-               break; 
-         
+               break;
+
           case 39:
                right=true;
                break;
-       
+
   }
   }
-  
+
 }
 
 class Boss extends Personnage{
-  
+
   constructor(nom, vie, posx, posy, vx, vy, l, h) {
-		this.nom = nom; 
+		this.nom = nom;
 		this.vie = vie; //0 à 100
 		this.positionX = posx;
         this.positionY = posy;
